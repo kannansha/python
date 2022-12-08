6d765204a7c974c7286c506990c3a979115421fd
kannansha <109779559+kannansha@users.noreply.github.com>
2022-12-08 19:10:24 +0530

    Add files via upload

---
+++ patterns(1).py
@@ -0,0 +1,8 @@
+m=int(input('Enter the row value:'))
+n=int(input('Enter the column value:'))
+k=1
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        print(k,end='')
+        k=k+1
+    print( )
