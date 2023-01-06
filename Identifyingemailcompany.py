738819d00fb2defd366e0919376b25397f8982b7
kannansha <109779559+kannansha@users.noreply.github.com>
2023-01-06 21:20:58 +0530

    Create Identifyingemailcompany.py

---
+++ Identifyingemailcompany.py
@@ -0,0 +1,7 @@
+email = input()
+if email.endswith("@gmail.com"):
+    print("You are using Google account")
+elif email.endswith("@yahoo.com"):
+    print("You are using Yahoo account")
+elif email.endswith("@adc.aditya.ac.in"):
+    print("You are using Aditya Educational Institute account")
