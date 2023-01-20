a81f0bc3eab4a5108139dc203bc02fd8bb5033a7
kannansha <109779559+kannansha@users.noreply.github.com>
2023-01-20 19:39:36 +0530

    Update removevowelsfromgivenstring.py

--- removevowelsfromgivenstring.py
+++ removevowelsfromgivenstring.py
@@ -2,7 +2,7 @@ s=input()
 n=len(s)
 res=''
 for i in range(1,n):
-    if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='O' or s[i]=='U':
+    if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or  s[i]=='O' or s[i]=='U':
         continue
     else:
         res=res+s[i]
